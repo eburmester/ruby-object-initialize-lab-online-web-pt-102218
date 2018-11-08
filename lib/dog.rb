862b3270c772c7d2c521c9257b2217819fae180d
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-08 18:53:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,7 @@
 class Dog 
-  def initialize(dog_name, breed_type=)
+  def initialize(dog_name, breed_type="Mutt")
     @name = dog_name
     @breed = breed_type
-    if breed_type == nil 
-      puts "Mutt"
-    end 
   end
   
 end
